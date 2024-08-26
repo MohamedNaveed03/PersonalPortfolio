d672c833d648bedd3a040ea04474eae8de5ba805
Mohamed Naveed <naveedmohamed870@gmail.com>
2024-08-26 16:05:28 +0530

    added functionlaity to download resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/circleimg.jpg'
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/circleimg.jpg';
+import { Link } from 'react-scroll';
+
 function Hero() {
   return (
     <div className='hero' id='hero-id'>
-      <img src={profile_img} alt=''></img>
+      <img src={profile_img} alt='Profile'></img>
       <h1><span>I'm Mohamed Naveed,</span> Software Engineer.</h1>
-      <p>Software Engineer from India with 2+ years of experience in Software Developement and Python Automation.</p>
+      <p>Software Engineer from India with 2+ years of experience in Software Development and Python Automation.</p>
       <div className='hero-action'>
-        <div className='hero-connect'>Connect With Me</div>
-        <div className='hero-resume'>My Resume</div>
+        <div className='hero-connect'>
+          <Link to="contact-id" smooth={true} duration={500}>Connect With Me</Link>
+        </div>
+        <div className='hero-resume'>
+          <a href="MohamedNaveed_SDE_Resume.pdf" download="Mohamed_Naveed_Resume.pdf" className="resume-link">
+            My Resume
+          </a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
